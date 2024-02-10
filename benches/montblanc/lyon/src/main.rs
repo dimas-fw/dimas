acc363a1e6fb20aecf2cdd49a5f8aab3540f4611
stepkun <stephan.kunz@kabelbw.de>
2024-02-10 13:15:13 +0100

    montblanc: lyon

--- benches/montblanc/lyon/src/main.rs
+++ benches/montblanc/lyon/src/main.rs
@@ -1,7 +1,33 @@
 // Copyright © 2024 Stephan Kunz
 
+//! The node 'lyon' subscribes to a float32 on the topic /amazon and publishes the rcieved value on topic /tigris
+//!
 //! This source is part of `DiMAS` implementation of Montblanc benchmark for distributed systems
 
-fn main() {
-	println!("Hello, world!");
+use std::sync::{Arc, RwLock};
+
+use dimas::prelude::*;
+
+#[derive(Debug)]
+struct AgentProps {}
+
+fn amazon_callback(ctx: &Arc<Context>, _props: &Arc<RwLock<AgentProps>>, message: &[u8]) {
+	let value: f32 = bitcode::decode(message).expect("should not happen");
+	let _ = ctx.publish("tigris", value);
+	println!("lyon propagates: {value:>11.6}");
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let properties = AgentProps {};
+	let mut agent = Agent::new(Config::default(), properties);
+
+	agent
+		.subscriber()
+		.put_callback(amazon_callback)
+		.msg_type("amazon")
+		.add()?;
+
+	agent.start().await;
+	Ok(())
 }
