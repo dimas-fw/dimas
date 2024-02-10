9554b084d691e1f37a7011a415632c77b0e03cc3
stepkun <stephan.kunz@kabelbw.de>
2024-02-10 13:01:51 +0100

    montblanc: cordoba

--- benches/montblanc/cordoba/src/main.rs
+++ benches/montblanc/cordoba/src/main.rs
@@ -1,7 +1,33 @@
 // Copyright © 2024 Stephan Kunz
 
+//! The node 'cordoba' publishes a float32 value every 100 ms on the topic /amazon
+//!
 //! This source is part of `DiMAS` implementation of Montblanc benchmark for distributed systems
 
-fn main() {
-	println!("Hello, world!");
+use dimas::prelude::*;
+use std::time::Duration;
+use rand::Rng;
+
+#[derive(Debug)]
+struct AgentProps {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let properties = AgentProps {};
+	let mut agent = Agent::new(Config::default(), properties);
+
+	agent
+		.timer()
+		.interval(Duration::from_millis(100))
+		.callback(|ctx, _props| {
+			let mut rng = rand::thread_rng();
+			let value = rng.gen_range(-999.99999f32..999.99999f32);
+			let _ = ctx.publish("amazon", value);
+			// just to see what value has be sent
+			println!("cordoba sent: {value:>11.6}");
+		})
+		.add()?;
+
+	agent.start().await;
+	Ok(())
 }
