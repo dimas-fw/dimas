db80c182a73f2a501ab3a961e05c22a83857bf66
stepkun <stephan.kunz@kabelbw.de>
2024-02-18 16:25:56 +0100

    montblanc: documentation

--- benches/montblanc/messages/src/lib.rs
+++ benches/montblanc/messages/src/lib.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
-// https://github.com/ros2/rcl_interfaces
-// https://github.com/ros2/
 
 //! all the messages needed for montblanc benchmark
-//!
+//! based on ros2 messages, see
+//! - <https://github.com/ros2/rcl_interfaces>
+//! - <https://github.com/ros2/common_interfaces>
+//! should be modernized and moved into a separate crate
 
 use bitcode::{Decode, Encode};
 
@@ -43,127 +44,206 @@ pub struct StringMsg {
 }
 
 /// Timestamp message
+/// Representing the time since 1.1.1970
 #[derive(Encode, Decode)]
 pub struct Timestamp {
+	/// seconds, valid over all int32 values
 	pub sec: i32,
+	/// The nanoseconds component if available, valid in the range [0, 1e9)
 	pub nanosec: u32,
 }
 
 /// Header message
+/// Standard metadata for higher-level stamped data types.
+/// This is generally used to communicate timestamped data
+/// in a particular coordinate frame.
 #[derive(Encode, Decode)]
 pub struct Header {
+	/// Timestamp of message creation
 	pub timestamp: Timestamp,
+	/// the frame id
 	pub frame_id: String,
 }
 
 /// Point message
+/// Contains the position of a point in free space
 #[derive(Encode, Decode)]
 pub struct Point {
+	/// x value
 	pub x: f64,
+	/// y value
 	pub y: f64,
+	/// z value
 	pub z: f64,
 }
 
 /// Quaternion message
+/// Represents an orientation in free space in quaternion form
 #[derive(Encode, Decode)]
 pub struct Quaternion {
+	/// x value
 	pub x: f64,
+	/// y value
 	pub y: f64,
+	/// z value
 	pub z: f64,
+	/// theta value
 	pub w: f64,
 }
 
 /// 3-Dimenional Vector message
+/// Represents a vector in free space
+/// This is semantically different than a point.
+/// A vector is always anchored at the origin.
+/// When a transform is applied to a vector, only the rotational component is applied.
 #[derive(Encode, Decode)]
 pub struct Vector3 {
+	/// x value
 	pub x: f64,
+	/// y value
 	pub y: f64,
+	/// z value
 	pub z: f64,
 }
 
 /// 3D-Vector with Header
+/// Represents a Vector3 with reference coordinate frame and timestamp
+/// Note that this follows vector semantics with it always anchored at the origin,
+/// so the rotational elements of a transform are the only parts applied when transforming.
 #[derive(Encode, Decode)]
 pub struct Vector3Stamped {
+	/// Timestamp and frame id
 	pub header: Header,
-	pub vectro: Vector3,
+	/// the Vector3 data
+	pub vector: Vector3,
 }
 
 /// Pose message
+/// Representation of pose in free space, composed of position and orientation
 #[derive(Encode, Decode)]
 pub struct Pose {
+	/// Position is a Point in free space
 	pub position: Point,
+	/// Orientation is a Quaternion
 	pub orientation: Quaternion,
 }
 
 /// Twist message
+/// Expresses velocity in free space broken into its linear and angular parts
 #[derive(Encode, Decode)]
 pub struct Twist {
+	/// Linear velocity
 	pub linear: Vector3,
+	/// Angular velocity
 	pub angular: Vector3,
-}
-
-/// Covariance message
-#[derive(Encode, Decode)]
-pub struct Covariance {
-	//pub values: f64[36],
 }
 
 /// Twist with Covariance message
 #[derive(Encode, Decode)]
 pub struct TwistWithCovariance {
+	/// Twist
 	pub twist: Twist,
-	pub covariance: Covariance,
+	/// Row-major representation of the 6x6 covariance matrix
+	/// The orientation parameters use a fixed-axis representation.
+	/// In order, the parameters are:
+	/// (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+	pub covariance: [f64; 36],
 }
 
 /// Twist with Covariance and Header message
 #[derive(Encode, Decode)]
 pub struct TwistWithCovarianceStamped {
+	/// Timestamp and frame id
 	pub header: Header,
+	/// Twist with Covariance
 	pub twist: TwistWithCovariance,
 }
 
-/// Wrench message
+/// Wrench message - represents force in free space, separated into its linear and angular parts
 #[derive(Encode, Decode)]
 pub struct Wrench {
+	/// Linear part is force
 	pub force: Vector3,
+	/// Angular part is torque
 	pub torque: Vector3,
 }
 
 /// Wrench with Header message
 #[derive(Encode, Decode)]
 pub struct WrenchStamped {
+	/// Timestamp and frame id
 	pub header: Header,
+	/// Wrench
 	pub wrench: Wrench,
 }
 
 /// Image message
+/// Contains an uncompressed image
+/// (0, 0) is at top-left corner of image
+/// +x should point to the right in the image
+/// +y should point down in the image
+/// +z should point into to plane of the image
 #[derive(Encode, Decode)]
 pub struct Image {
+	/// Header timestamp should be acquisition time of image
+	/// Header frame_id should be optical frame of camera
+	/// origin of frame should be optical center of cameara
+	/// If the frame_id here and the frame_id of the CameraInfo
+	/// message associated with the image conflict the behavior is undefined
 	pub header: Header,
+	/// Image height, that is, number of rows
 	pub height: u32,
+	/// Image width, that is, number of columns
 	pub width: u32,
-	//pub encoding: String,
+	/// Encoding of pixels -- channel meaning, ordering, size 
+	/// see: https://github.com/ros2/common_interfaces/blob/rolling/sensor_msgs/include/sensor_msgs/image_encodings.hpp
+	pub encodings: String,
+	/// Is this data bigendian?
 	pub is_bigendian: bool,
+	/// Full row length in bytes
 	pub step: u32,
-	//pub data: u8[],   // size is height*step (width is only giving num of pixels not the bytes/pixel step = width * bytes/pixel)
+	/// Actual matrix data, size is (step * rows) width is only giving num of pixels not the bytes/pixel step = width * bytes/pixel
+	pub data: Vec<u8>,
 }
 
 /// Laser Scan message
+/// Single scan from a planar laser range-finder
+/// If you have another ranging device with different behavior (e.g. a sonar
+/// array), please find or create a different message, since applications
+/// will make fairly laser-specific assumptions about this data
 #[derive(Encode, Decode)]
 pub struct LaserScan {
+	/// timestamp in the header is the acquisition time of
+	/// the first ray in the scan.
+	/// In frame frame_id, angles are measured around
+	/// the positive Z axis (counterclockwise, if Z is up)
+	/// with zero angle being forward along the x axis
 	pub header: Header,
+	/// Start angle of the scan [rad]
 	pub angle_min: f32,
+	/// End angle of the scan [rad]
 	pub angle_max: f32,
+	/// Angular distance between measurements [rad]
 	pub angle_increment: f32,
+	/// Time between measurements [seconds] - if your scanner
+	/// is moving, this will be used in interpolating position
+	/// of 3d points
 	pub time_increment: f32,
+	/// Time between scans [seconds]
 	pub scan_time: f32,
+	/// Minimum range value [m]
 	pub range_min: f32,
+	/// Maximum range value [m]
 	pub range_max: f32,
-	//pub ranges: f32[],
-	//pub intensities: f32[],
+	/// Range data [m] (Note: values < range_min or > range_max should be discarded)
+	pub ranges: Vec<f32>,
+	/// Intensity data [device-specific units].
+	/// If your device does not provide intensities, please leave the array empty.
+	pub intensities: Vec<f32>,
 }
 
 /// Data Type message
+/// Definitions for the type of data used in `PointField`
 #[derive(Encode, Decode)]
 pub enum DataType {
 	/// Int8 type
@@ -184,25 +264,47 @@ pub enum DataType {
 	Float64,
 }
 
-/// Point Filed message
+/// Point Field message
+/// Holds the description of one point entry in the `PointCloud2` message format
+/// Common Point Field names are x, y, z, intensity, rgb, rgba
 #[derive(Encode, Decode)]
 pub struct PointField {
+	/// Name of field
 	pub name: String,
+	/// Offset from start of point struct
 	pub offset: u32,
+	/// Data type enumeration, see `DataType`
 	pub datatype: DataType,
+	/// How many elements in the field
 	pub count: u32,
 }
 
 /// Point Cloud message
+/// This message holds a collection of N-dimensional points, which may
+/// contain additional information such as normals, intensity, etc. The
+/// point data is stored as a binary blob, its layout described by the
+/// contents of the "fields" array.
+/// The point cloud data may be organized 2d (image-like) or 1d (unordered).
+/// Point clouds organized as 2d images may be produced by camera depth sensors
+/// such as stereo or time-of-flight.
 #[derive(Encode, Decode)]
 pub struct PointCloud2 {
+	/// Time of sensor data acquisition, and the coordinate frame ID (for 3d points)
 	pub header: Header,
-	pub height: u32, //
-	pub width: u32,  //
-	//pub fields: PointField[],       //
-	pub is_bigendian: bool, // Is this data bigendian?
-	pub point_step: u32,    // Length of a point in bytes
-	pub row_step: u32,      // Length of a row in bytes
-	//pub data: u8[],                 // Size is (row_step*height)
-	pub is_dense: bool, // True if there are no invalid points
+	/// Height of the point cloud. If the cloud is unordered, height is 1.
+	pub height: u32,
+	/// Width of the point cloud. If the cloud is unordered, width is the length of the point cloud.
+	pub width: u32,
+	/// Describes the channels and their layout in the binary data blob
+	pub fields: Vec<PointField>,
+	/// Is this data bigendian?
+	pub is_bigendian: bool,
+	/// Length of a point in bytes
+	pub point_step: u32,
+	/// Length of a row in bytes
+	pub row_step: u32,
+	/// Size is (row_step*height)
+	pub data: Vec<u8>,
+	/// True if there are no invalid points
+	pub is_dense: bool,
 }
