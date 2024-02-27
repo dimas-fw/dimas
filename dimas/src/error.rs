8129d2a864e712a09376692c75081f286108d09d
stepkun <stephan.kunz@kabelbw.de>
2024-02-27 17:51:15 +0100

    wip: error handling

--- dimas/src/error.rs
+++ dimas/src/error.rs
@@ -2,22 +2,19 @@
 
 // region:    --- types
 /// Enables simplified usage of Result with crates Error type
-pub type Result<T> = core::result::Result<T, Error>;
-
-/// crates temporary Error type
-pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = core::result::Result<T, DimasError>;
 // endregion: --- types
 
 // region:    --- Error
-//#[derive(thiserror::Error, Debug, Clone)]
-//pub enum Error {
-//    #[error("Generic {0}")]
-//    Generic(String),
-//}
-//
-//impl Default for Error {
-//    fn default() -> Self {
-//        Self::Generic("error".to_string())
-//    }
-//}
+#[derive(thiserror::Error, Debug, Clone)]
+pub enum DimasError {
+    #[error("Generic {0}")]
+    Generic(String),
+}
+
+impl Default for DimasError {
+    fn default() -> Self {
+        Self::Generic("error".to_string())
+    }
+}
 // endregion: --- Error
